4e9bd623a5a74343347921413dbae1ce0b5caddd
Nick Seidenman <nick@seidenman.net>
2020-01-27 22:49:38 +1100

    Started adding templates.

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+    
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('main.html', name=name)    
